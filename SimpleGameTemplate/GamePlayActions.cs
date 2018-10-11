8bf9653643e87059be5a6ab591725d17857b0231
Juha Liias <juha.liias@gmail.com>
2018-10-11 20:01:52 +0300

    Added few lines to imitate score/highscore accessing

--- SimpleGameTemplate/GamePlayActions.cs
+++ SimpleGameTemplate/GamePlayActions.cs
@@ -11,20 +11,36 @@ using UnityEngine;
 public class GamePlayActions : MonoBehaviour {
 
 	// Set this true when ready to switch to next scene
+	// public, so you can switch it on and off from Unity inspector too
 	public bool sceneDone;
+
+	private bool highScoreReady;
+
+	private int score;
 
 	// Use this for initialization
 	void Start () {
 		sceneDone = false;
+		highScoreReady = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		// Replace following with your gamelogic controls as you wish
-		// You need set sceneDone as 'true' when ready to move next level/scene
+		// Replace following with your gamelogic controls as you wish.
+		// You need set sceneDone as 'true' when ready to move next level/scene.
+		// Also, highScoreReady boolean is also used here to test GameManager
+		// highscore mechanisms
 		if (Input.GetMouseButtonDown (0)) {
 			Debug.Log ("Ready for next level!");
+			if (!sceneDone && !highScoreReady) {
+				score =  Random.Range (1, 10);
+				// Access variable from GameManager singleton instance
+				if (score > GameManager.instance.highScore) {
+					GameManager.instance.highScore = score;
+					Debug.Log ("highscore: " + GameManager.instance.highScore);
+				}
+			}
 			sceneDone = true;
 		}
 		// end of block
