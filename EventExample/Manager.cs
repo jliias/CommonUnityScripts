81381ac117c4a237f28a56e00b9a9758a823e30a
Juha Liias <juha.liias@gmail.com>
2019-11-17 15:57:39 +0200

    events example added

---
+++ EventExample/Manager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    // Awake is called after all objects are initialized
+    private void Awake()
+    {
+        // Subscribe PlayerHit event
+        TestPlayer.PlayerHit += GetDamage;
+    }
+
+   // GetDamage is called when PlayerHit event is detected 
+    private void GetDamage(int damage) {
+        Debug.Log("Got " + damage + " damage!");
+        // Do something when player is getting damage
+        // add your actions here
+    }
+
+    // OnDisable is called when behavior becomes disabled
+    private void OnDisable()
+    {
+        // UnSubscribe to avoid memory leak
+        TestPlayer.PlayerHit -= GetDamage;
+    }
+}
+
---
+++ EventExample/TestPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+public class TestPlayer : MonoBehaviour
+{
+    // Define event when player is hit
+    public static event Action<int> PlayerHit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Left mouse button will trigger PlayerHit event
+        if (Input.GetMouseButtonDown(0)) {
+            PlayerHit(UnityEngine.Random.Range(1, 5));
+        }        
+    }
+
+}
